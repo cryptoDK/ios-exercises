a37fe31f768ae1ce90abb91d94ad7df28405c864
cryptoDK <cryptoDK@gmail.com>
2015-04-04 15:55:51 -0700

    Completed loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,30 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    if (number == otherNumber) {
+        return [NSString stringWithFormat:@"%ld", (long)number];
+        
+    }
+    
+    NSInteger smallNumber, highNumber;
+    
+    if (number < otherNumber) {
+        smallNumber = number;
+        highNumber = otherNumber;
+    } else {
+        smallNumber = otherNumber;
+        highNumber = number;
+    }
+    
+    NSMutableString *returnString = [NSMutableString string];
+    
+    for (NSInteger i = smallNumber; i <= highNumber; i++) {
+        [returnString appendFormat:@"%ld", (long)i];
+    }
+    
+    return returnString;
+    
 }
 
 @end
