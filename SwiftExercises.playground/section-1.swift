fd47c6cb870905cb8c352231b8b6bb347dcd352c
cryptoDK <cryptoDK@gmail.com>
2015-04-06 21:50:56 -0700

    Swift vs Objective-C

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -1,9 +1,7 @@
 import UIKit
 
 /*
-
 Strings
-
 */
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
@@ -11,34 +9,34 @@ func favoriteCheeseStringWithCheese(cheese: String) -> String {
     return cheese
 }
 
-let fullSentence = favoriteCheeseStringWithCheese("cheddar")
+let fullSentence = favoriteCheeseStringWithCheese("My favorite cheese is cheddar.")
 // Make fullSentence say "My favorite cheese is cheddar."
 
 /*
-
 Arrays & Dictionaries
-
 */
 
-let numberArray = [1, 2, 3, 4]
+let numberArray = [1, 2, 3, 4, 5]
 // Add 5 to this array
 // WORK HERE
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
 
 /*
-
 Loops
-
 */
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
 
+1...9
+
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+
+1..<10
 
 let worf = [
     "name": "Worf",
@@ -58,7 +56,10 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+
+    
+    
+    return characters[]
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -66,9 +67,7 @@ let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
 favoriteDrinks
 
 /*
-
 Functions
-
 */
 
 // Make a function that inputs an array of strings and outputs the strings separated by a semicolon
@@ -77,15 +76,31 @@ let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
 
+func printHarrysMovieTitle(name: String = "the Sorcerer's Stone") {
+    println("Harry Potter and \(name)")
+}
+
 let expectedOutput = "milk;eggs;bread;challah"
 
 /*
-
 Closures
-
 */
 
 let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+
+
+
+var reversed = sorted (cerealArray,
+    { (s1 : String, s2 : String) -> Bool in
+        return s1 > s2
+    }
+    )
+println(reversed)
+
+var reverseOrder3 = sorted(cerealArray, >)
+
+
+var sortedNames = sorted(cerealArray, <)
